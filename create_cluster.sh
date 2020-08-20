3f21ddf61455914c5afeb42f541759f0d0b343b6
Roberto Meléndez <62077107+rcmelendez@users.noreply.github.com>
2020-08-20 07:50:27 -0400

    Delete create_cluster.sh

--- create_cluster.sh
+++
@@ -1,5 +0,0 @@
-# CloudFormation command to create an AWS EKS cluster
-eksctl create cluster --config-file capstone-cluster.yaml
-
-# Create kubectl config file to store cluster info
-aws eks --region us-east-2 update-kubeconfig --name capstone
